aeb6229f7133e582d0bb78004233a9356f6e89f1
Vivek Bhookya <v.bhookya98@gmail.com>
2022-09-14 13:09:23 -0500

    [Popup] User can save hourlyRate to storage

--- popup/PopupFunctionality.js
+++ popup/PopupFunctionality.js
@@ -1,11 +1,28 @@
 function saveRateToStorage() {
-  let rate = document.getElementById("btn_save_rate").value;
+  console.log("cllicked save");
+  let rate = document.getElementById("input_hourly_rate").value;
 
   if (rate) {
-    localStorage.setItem("hourlyRate", rate); // this doesnt seem to work. switch to chrome storage sync
+    chrome.storage.sync.set({ hourlyRate: rate }, function () {
+      console.log("rate saved?"); // replace with some kind of visual feedback, like a Toast from Android platform
+    });
   }
 }
 
 let button_save_rate = document.getElementById("btn_save_rate");
-
 button_save_rate.addEventListener("click", saveRateToStorage);
+
+chrome.storage.sync.get("hourlyRate", function (returnedObject) {
+  if (!returnedObject) {
+    return;
+  }
+
+  let rate = returnedObject["hourlyRate"];
+  if (rate) {
+    document.getElementById("input_hourly_rate").value = parseFloat(rate);
+  } else {
+    console.error(
+      "hourlyRate property is missing, but the object exists in storage."
+    );
+  }
+});

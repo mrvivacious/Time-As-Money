289c209296bca127b984db1944ea04586ee6cc1d
Vivek Bhookya <v.bhookya98@gmail.com>
2022-09-15 16:10:07 -0500

    [Duration #4] UI + logic to save workday length (hours)

--- chromeExtension/popup/PopupFunctionality.js
+++ chromeExtension/popup/PopupFunctionality.js
@@ -1,5 +1,5 @@
 function saveRateToStorage() {
-  console.log("cllicked save");
+  console.log("cllicked saveRate");
   let rate = document.getElementById("input_hourly_rate").value;
 
   if (rate) {
@@ -25,6 +25,39 @@ chrome.storage.sync.get("hourlyRate", function (returnedObject) {
   } else {
     console.error(
       "hourlyRate property is missing, but the object exists in storage."
+    );
+  }
+});
+
+function saveDurationToStorage() {
+  console.log("cllicked saveDuration");
+  let duration = document.getElementById("input_workday_duration").value;
+
+  if (duration) {
+    // TODO put below lines in a data interface class of some sort?
+    // also, workdayDuration and hourlyRate would be better as keys to a user object rather than individual items
+    chrome.storage.sync.set({ workdayDuration: duration }, function () {
+      console.log("duration saved?"); // replace with some kind of visual feedback, like a Toast from Android platform
+    });
+  }
+}
+
+let button_save_duration = document.getElementById("btn_save_duration");
+button_save_duration.addEventListener("click", saveDurationToStorage);
+
+// TODO put below lines in a data interface class of some sort?
+chrome.storage.sync.get("workdayDuration", function (returnedObject) {
+  if (!returnedObject) {
+    return;
+  }
+
+  let duration = returnedObject["workdayDuration"];
+  if (duration) {
+    document.getElementById("input_workday_duration").value =
+      parseFloat(duration);
+  } else {
+    console.error(
+      "workdayDuration property is missing, but the object exists in storage."
     );
   }
 });
